07bb4241ab1147caff9d7f533b2a59fec035125d
Thunder Master <thndrmaster@gmail.com>
2019-06-17 14:39:52 -0300

    Criando A Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "home#index"
+  
+  authenticated :user do
+    root 'dashboard#index', as: :authenticated_root
+  end
+  
+  unauthenticated :user do
+    root to: "home#index"
+  end
 end
