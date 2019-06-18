e0636eb6d38eb954182432f9e7efa958d08e5100
Thunder Master <thndrmaster@gmail.com>
2019-06-18 09:26:40 -0300

    Favoritando Albums Artistas e Musicas

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   
   authenticated :user do
-    root 'dashboard#index', as: :authenticated_root
+    resources :favorites, only: :index
+    root to: "dashboard#index", as: :authenticated_root
     resources :search, only: [:index, :new], as: :searches
-    resources :artists, only: :show
     resources :categories, only: :show
+    resources :artists, only: :show
     resources :albums, only: :show
+    resources :songs, only: [] do
+      post "/favorite", to: "favorites#create", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+      delete "/favorite", to: "favorites#destroy", on: :member, defaults: { format: :js, favoritable_type: 'Song' }
+    end
   end
-  
+
   unauthenticated :user do
     root to: "home#index"
   end
